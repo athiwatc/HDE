a29bfec678bb393ed8b9dfe248dfe06722426640
Athiwat Chunlakhan <athiwatc@gmail.com>
2011-04-23 18:22:01 +0700

    Add ivar and NSXMLPArserDelegate

--- HDE/HDEAppDelegate.h
+++ HDE/HDEAppDelegate.h
@@ -8,11 +8,28 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface HDEAppDelegate : NSObject <NSApplicationDelegate> {
+@interface HDEAppDelegate : NSObject <NSApplicationDelegate, NSXMLParserDelegate> {
 @private
     NSWindow *window;
+    NSTextView *output;
+    NSTextField *matchID;
+    NSString* scratchFolder;
+    NSMutableString *urlToServerLog;
+    NSString *currentKey;
+    NSMutableString *currentValue;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSTextView *output;
+@property (assign) IBOutlet NSTextField *matchID;
+@property (retain) NSString *scratchFolder;
+@property (retain) NSMutableString *urlToServerLog;
+@property (retain) NSString *currentKey;
+@property (retain) NSMutableString *currentValue;
+
+-(IBAction) ParserButton:(id)sender;
+-(NSFileWrapper*)unzip:(NSData*)zipData;
+-(NSString*)getValueFrom:(NSArray*)array at:(int)position;
+-(NSDictionary*)parseLog: (NSString*)log;
 
 @end
