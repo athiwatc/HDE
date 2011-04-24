703dc2d209d55951bf819dfdc690b6e066992bcb
Athiwat Chunlakhan <athiwatc@gmail.com>
2011-04-24 19:36:28 +0700

    Optimize import.

--- HDE/main.m
+++ HDE/main.m
@@ -6,9 +6,6 @@
 //  Copyright 2011 Kasetsart University. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     return NSApplicationMain(argc, (const char **)argv);
 }
